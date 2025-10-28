175f6b80d97a82f1dd6f3c3d17f7b0de5241a101
hohoemi-rabo <rabo.hohoemi@gmail.com>
2025-10-28 15:47:07 +0900

    ⬇️ tailwindcssバージョンダウン実装

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "system-ui", "sans-serif"],
+        mono: [
+          "var(--font-geist-mono)",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "monospace",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
